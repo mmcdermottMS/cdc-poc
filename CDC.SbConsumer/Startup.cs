5108bca056292d98cda7f43885d2b1841abb20b3
Mike McDermott <mmcdermott@microsoft.com>
2022-07-06 17:42:58 -0500

    removed secret

--- CDC.SbConsumer/Startup.cs
+++ CDC.SbConsumer/Startup.cs
@@ -14,7 +14,7 @@ namespace CDC
             builder.Services.AddApplicationInsightsTelemetryWorkerService();
             builder.Services.AddSingleton<ICosmosDbService>(new CosmosDbService(new CosmosClient(
                 accountEndpoint: "https://cdc-poc-wus-cdb.documents.azure.com",
-                authKeyOrResourceToken: "niP9Ahk5dL6VVpm1DADn8C1AHSsZCfWgTJqZ67ULLz28E3WBoAvw9O0IufQ45fNkJeKAiTAWwap2kjeZ5RTcSg=="
+                authKeyOrResourceToken: ""
             )));
         }
     }
