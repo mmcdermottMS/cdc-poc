24dc838bc9ce4f6ba2469b60520a84d62aaf7a0a
Mike McDermott <mmcdermott@microsoft.com>
2022-07-07 09:01:20 -0500

    clean up

--- CDC.EhProducer/CosmosInitializer.cs
+++ CDC.EhProducer/CosmosInitializer.cs
@@ -8,7 +8,7 @@ namespace CDC.EhProducer
         {
             using CosmosClient cosmosClient = new CosmosClient(
                 accountEndpoint: "https://cdc-poc-wus-cdb.documents.azure.com",
-                authKeyOrResourceToken: "niP9Ahk5dL6VVpm1DADn8C1AHSsZCfWgTJqZ67ULLz28E3WBoAvw9O0IufQ45fNkJeKAiTAWwap2kjeZ5RTcSg=="
+                authKeyOrResourceToken: ""
             );
 
             var databaseResponse = await cosmosClient.CreateDatabaseIfNotExistsAsync(id: "Customers");
